1e45e2115d25bddc862810733e1b79fe5e9e92b5
NCLYAnup <choudhuryanup8@gmail.com>
2021-05-28 09:52:05 +0530

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+const allCategories = ["all", ...new Set(items.map((item)=> item.category))];
+
+function App() {
+  const [menuItems, setMennuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+
+  const filterItems = (category) => {
+    if(category === "all"){
+      setMennuItems(items);
+      return;
+    }
+    const newItems = items.filter((item)=> item.category === category)
+    setMennuItems(newItems); 
+  }
+
+  return ( 
+     <main>
+     <section className="menu section">
+    <div className="title">
+        <h2>our menu</h2>
+        <div className='underline'>
+        </div>
+    </div>
+    <Categories categories={categories} filterItems=
+    {filterItems} />
+    <Menu items={menuItems} />
+    </section>
+  </main>
+  );
+}
+
+export default App;
